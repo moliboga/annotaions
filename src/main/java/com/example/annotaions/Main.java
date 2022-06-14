da0e5023d41cafcaae42621daee2ccc581091e9f
Valery Stopen <stopen.lera@gmail.com>
2022-06-14 19:50:35 +0300

    add values of all annotation marked fields

--- src/main/java/com/example/annotaions/Main.java
+++ src/main/java/com/example/annotaions/Main.java
@@ -1,16 +1,47 @@
 package com.example.annotaions;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
-@SpringBootApplication
 public class Main {
 
     @CustomAnnotation(operation = "add", firstArgument = 9, secondArgument = 0)
-    private final int number = 1;
+    private static final int number = 1;
+
+    @AddAll
+    private static final int firstField = 12;
+
+    @AddAll
+    private static final int secondField = 13;
+
+    @AddAll
+    private static final int thirdField = 5;
+
 
     public static void main(String[] args) throws NoSuchFieldException {
         AnnotationExecute annotationExecute = new AnnotationExecute();
         System.out.println(annotationExecute.execute());
+        System.out.println("-------------------------");
+
+        int sum = Arrays.stream(Main.class.getDeclaredFields())
+                .filter(field ->
+                        Arrays.stream(field.getAnnotations())
+                                .toList()
+                                .stream()
+                                .filter(annotation ->
+                                        annotation
+                                                .annotationType()
+                                                .equals(AddAll.class))
+                                .toList()
+                                .size() > 0)
+                .mapToInt(field -> {
+                    try {
+                        return (int) field.get(null);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .sum();
+        System.out.println(sum);
     }
 
 }
